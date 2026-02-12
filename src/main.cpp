e56a0e4962b407ab482cd269eae7e5de17c5d5f0
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-02-12 17:04:06 +0000

    Set color from binary literal

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <bn_core.h>
 #include <bn_log.h>
 #include <bn_string.h>
+#include <bn_backdrop.h>
+#include <bn_color.h>
 
 #include "base_conv.h"
 
 int main() {
     bn::core::init();
 
+    unsigned int color = 0b0'00000'11111'00000;
+
     BN_LOG("Hey!");
+
+    bn::backdrop::set_color(bn::color(color));
 
     while(true) {
         bn::core::update();
